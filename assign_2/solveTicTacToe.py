a764a7c7fcf30e6b7e1806bb143dc9eba4137aad
tngan <tonynwk919@gmail.com>
2016-04-15 16:42:23 +0800

    Finish end logic and build structure for minimax

--- assign_2/solveTicTacToe.py
+++ assign_2/solveTicTacToe.py
@@ -1,5 +1,4 @@
-"""
-Solve Tic Tac Toe misere version
+""" Solve Tic Tac Toe misere version
 
 Idea:
 
@@ -20,17 +19,35 @@ while(game is not end) {
 }
 
 """
+import copy
+
 class Game:
   def __init__(self):
     self.board = (['0','1','2','3','4','5','6','7','8'], ['0','1','2','3','4','5','6','7','8'], ['0','1','2','3','4','5','6','7','8'])
-    self.aiboard = [] # keep tracking
-    self.playerboard = [] # keep tracking
+    #self.aiboard = [] # keep tracking
+    #self.playerboard = [] # keep tracking
     self.playerIndex = 0
     self.status = [1, 1, 1]
-    self.end = 0
+    self.depth = 2
+
+  def copy(self):
+    g = Game()
+    g.board = self.board
+    g.status = self.status
+    g.depth = self.depth
+    g.playerIndex = self.playerIndex
+    return g
 
   def isEnd(self):
-    #TODO
+    # End only when all three boards are dead
+    if len([i for i, x in enumerate(self.status) if x == 0]) == 3:
+      # Trick: See the bottom of main in loop, check end is run after the player index is increased
+      if self.playerIndex == 1:
+        print "You win the AI !" 
+      else:
+        print "AI win the game !"
+      return True
+    
     return False 
 
 #
@@ -55,32 +72,33 @@ def parseMutate(status, board, input):
   return True 
 
 def eliminateDeadBoard(board, status):
-  for id in range(0, 3):
-    # Check if the board is not dead
-    if status[id] != 0:
-      # Dead check in 3 rows
-      for row in range(0, 3):
-        if board[id][3*row] == 'X' and board[id][3*row+1] == 'X' and board[id][3*row+2] == 'X':
-          status[id] = 0
-          return
-      # Dead check in 3 columns
-      for col in range(0, 3):
-        if board[id][col] == 'X' and board[id][col+3] == 'X' and board[id][col+6] == 'X':
-          status[id] = 0
-          return
-      # Dead check in 2 diagonal
-      if board[id][4] == 'X' and ((board[id][0] == 'X' and board[id][8] == 'X') or (board[id][2] == 'X' and board[id][6] == 'X')):
+  for id in [i for i, x in enumerate(status) if x == 1]:
+    # Dead check in 3 rows
+    for row in range(0, 3):
+      if board[id][3*row] == 'X' and board[id][3*row+1] == 'X' and board[id][3*row+2] == 'X':
         status[id] = 0
         return
+    # Dead check in 3 columns
+    for col in range(0, 3):
+      if board[id][col] == 'X' and board[id][col+3] == 'X' and board[id][col+6] == 'X':
+        status[id] = 0
+        return
+    # Dead check in 2 diagonal
+    if board[id][4] == 'X' and ((board[id][0] == 'X' and board[id][8] == 'X') or (board[id][2] == 'X' and board[id][6] == 'X')):
+      status[id] = 0
+      return
 
 def update (game, playerIndex):
   if playerIndex == 0:
-    print 'AI: '
+    aiOutput = brain(copy.deepcopy(game))
+    parseMutate(game.status, game.board, aiOutput)
+    print "AI: {0}".format(aiOutput)
   else:
     isValidInput = False 
     while not isValidInput:
       isValidInput = parseMutate(game.status, game.board, raw_input('Your move: '))
-    eliminateDeadBoard(game.board, game.status)   
+  
+  eliminateDeadBoard(game.board, game.status)   
 
 def draw (status, board):
   head = ''
@@ -97,19 +115,70 @@ def draw (status, board):
 
 #############################################################################################
 def brain (game):
-  return ""
+  # Do not want to mutate the original reference
+  return minimax(game, 0, 0)[0] 
 
-def minimax ():
-  return
+def minimax(game, agentIdx, layer):
+    if 2 <= agentIdx:
+      agentIdx = 0 # Pacman's turn again
+      layer += 1 
 
-def eval ():
-  return
+    if layer == game.depth:
+      return (0, evaluationFunction(game))
+     
+    if agentIdx == 0:
+      return eval('MAX', game, agentIdx, layer)
+    else:
+      return eval('MIN', game, agentIdx, layer)
 
-def getLegalMoves ():
-  return
+def eval(type, game, agentIdx, layer):
 
-def evaluationFunction ():
-  return
+    # Terminal node checking, no legal move anymore
+    if len(getLegalMoves(game.board, game.status)) == 0:
+      return (0, evaluationFunction(game))
+
+    if type == 'MIN':
+      # Worst max is inf (Initialize)
+      v = ("unknown", float("inf"))
+    else:
+      # Worst max is -inf (Initialize)
+      v = ("unknown", -float("inf"))
+
+    for action in getLegalMoves(game.board, game.status):
+
+      result = minimax(generateSuccessor(game, action), agentIdx+1, layer)[1]
+
+      if type == 'MIN':
+        nextv = min(v[1], result)
+      else:
+        nextv = max(v[1], result)
+
+      if nextv is not v[1]:
+        v = (action, nextv)
+
+    return v 
+
+def getLegalMoves (board, status):
+  # Return something like { "A": [], "B": [], "C": [] } 
+  # If the board is already dead, it won't show the board id 
+  # result = {}
+  result = []
+  for id in [i for i, x in enumerate(status) if x == 1]:
+    result += ["{0}{1}".format(chr(id+65), i) for i, x in enumerate(board[id]) if x != 'X']
+  return result
+
+def generateSuccessor(game, action):
+  # Check whether the ord is already occupied
+  index = int(action[1])
+  id = ord(action[0]) - 65
+  # No need to check if the board is not dead and is occupied
+  # Mutate the original board globally
+  game.board[id][index] = 'X'
+  return game
+
+def evaluationFunction (game):
+
+  return 1
 ##############################################################################################
 
 # Start main
